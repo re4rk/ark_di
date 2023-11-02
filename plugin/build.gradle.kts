78194bcfb74708f8a356911cf3ae4ebaac48dfbe
re4rk <37167652+re4rk@users.noreply.github.com>
2023-11-02 13:43:39 +0900

    feat(plugin): 힐트 테스트 코드 추

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,6 +17,15 @@ dependencies {
 
     // android gradle plugin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+
+    // junit
+    implementation("junit:junit:4.13.2")
+
+    // assertj
+    implementation("org.assertj:assertj-core:3.21.0")
+
+    // javassist
+    testImplementation("org.javassist:javassist:3.27.0-GA")
 }
 
 gradlePlugin {
